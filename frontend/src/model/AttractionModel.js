6c667989ac2d974d1e6df2d7c265a1995918b02e
Luis Aguilar <luisaguilar159@gmail.com>
2024-01-18 17:27:12 -0800

    Update create attraction #27
    Add class for attraction creation, update attraction form onChange method, add sweetalert2 library to confirm end-user choice

---
+++ frontend/src/model/AttractionModel.js
@@ -0,0 +1,17 @@
+class AttractionModel {
+    constructor() {
+        this.id = "";
+        this.name = "";
+        this.description = "";
+        this.comments = "";
+        this.deleted = false;
+        this.city_id = "";
+        this.country_id = "";
+        this.createdby = "";
+        this.createddatetime = "";
+        this.lastupdatedby = "";
+        this.lastupdateddatetime = "";
+    }
+}
+
+export default AttractionModel;
--- frontend/src/pages/Attraction/AttractionCreate.js
+++ frontend/src/pages/Attraction/AttractionCreate.js
@@ -11,62 +11,66 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-// Services and helpers
+// Services, classes, and helpers
 import AttractionService from '../../services/AttractionService';
 import CityService from '../../services/CityService';
 import Constants from '../../Constants';
+import AttractionModel from '../../model/AttractionModel';
+
+// Sweetalert2 library
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
 function AttractionCreate() {
 
     // State variables
     const [lstCities, setLstCities] = useState([]);
     const [lstCountries, setLstCountries] = useState([]);
-    const [attractionName, setAttractionName] = useState("");
-    const [attractionDesc, setAttractionDesc] = useState("");
-    const [attractionComm, setAttractionComm] = useState("");
-    const [attractionCityId, setAttractionCityId] = useState("");
-    const [attractionCountryId, setAttractionCountryId] = useState("");
+    const [attractionForm, setAttractionForm] = useState(new AttractionModel());
 
     // Navigation variables
     let navigate = useNavigate();
+
+    // Sweetalert2 variables
+    const MySwal = withReactContent(Swal);
 
     // useEffect(() => {       
     //     // Here will go the getAllCountries service method
     // }, []);
 
     const createAttraction = () => {
-        let newAttraction = {
-            name: attractionName,
-            description: attractionDesc,
-            comments: attractionComm,
-            city_id: attractionCityId,
-            country_id: attractionCountryId
-        }        
-        AttractionService.createAttraction(newAttraction).then((result) => {    
-            // Pending to add swal library for animation        
-            console.log('Attraction created:', result);
-            setAttractionName("");
-            setAttractionDesc("");
-            setAttractionComm("");
-            setAttractionCityId("");
-            setAttractionCountryId("");
-        });
+        MySwal.fire({
+            title: 'Are you sure?',
+            text: "This action will create an attraction.",
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Create',
+            confirmButtonColor: '#0d6efd'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                AttractionService.createAttraction(attractionForm).then((result) => {                    
+                    console.log('Attraction created:', result);
+                    setAttractionForm(new AttractionModel());
+                    MySwal.fire({ title: 'Created!', text: 'Attraction was successfuly created', icon: 'success' });
+                });                
+            }
+        })        
     }
 
-    const redirectRoute = (path) => {        
+    const redirectRoute = (path) => {
         navigate(path);
     }
 
     const filterCityByCountry = (country_code) => {
-        CityService.getCityByCountryCode(country_code).then((result) => {            
+        CityService.getCityByCountryCode(country_code).then((result) => {
             setLstCities(result);
         });
     }
 
-    const onChangeCountryList = (val) => {        
-        setAttractionCountryId(val);
+    const onChangeCountryList = (val) => {
         // INIT ------------------------------
-        // Temporal code until Country Management (getAllCountries) is completed       
+        // Temporal code until Country Management (getAllCountries) is completed
         let countryCode = "";
         if (val === '176') {
             countryCode = 'PE';
@@ -78,7 +82,37 @@ function AttractionCreate() {
         }
         // END ------------------------------
         filterCityByCountry(countryCode);
-    }    
+    }
+
+    const handleAttractionChange = (event) => {
+        let tmpObj = new AttractionModel();
+        tmpObj.name = attractionForm.name;
+        tmpObj.description = attractionForm.description;
+        tmpObj.comments = attractionForm.comments;
+        tmpObj.city_id = attractionForm.city_id;
+        tmpObj.country_id = attractionForm.country_id;
+        switch (event.target.id) {
+            case 'attraction_name':
+                tmpObj.name = event.target.value;
+                break;
+            case 'attraction_desc':
+                tmpObj.description = event.target.value;
+                break;
+            case 'attraction_comments':
+                tmpObj.comments = event.target.value;
+                break;
+            case 'attraction_countryid':
+                tmpObj.country_id = event.target.value;
+                onChangeCountryList(event.target.value);
+                break;
+            case 'attraction_cityid':
+                tmpObj.city_id = event.target.value;
+                break;
+            default:
+                break;
+        }
+        setAttractionForm(tmpObj);
+    }
 
     return (
         <Container className='my-4'>
@@ -90,19 +124,19 @@ function AttractionCreate() {
                     <Form>
                         <Form.Group className='mb-2'>
                             <Form.Label>Name</Form.Label>
-                            <Form.Control type='text' onChange={(event) => { setAttractionName(event.target.value) }} value={attractionName}></Form.Control>
+                            <Form.Control id='attraction_name' type='text' onChange={handleAttractionChange} value={attractionForm.name}></Form.Control>
                         </Form.Group>
                         <Form.Group className='mb-2'>
                             <Form.Label>Description</Form.Label>
-                            <Form.Control as="textarea" rows={3} onChange={(event) => { setAttractionDesc(event.target.value) }} value={attractionDesc}></Form.Control>
+                            <Form.Control id='attraction_desc' as="textarea" rows={3} onChange={handleAttractionChange} value={attractionForm.description}></Form.Control>
                         </Form.Group>
                         <Form.Group className='mb-2'>
                             <Form.Label>Comments</Form.Label>
-                            <Form.Control as="textarea" rows={3} onChange={(event) => { setAttractionComm(event.target.value) }} value={attractionComm}></Form.Control>
+                            <Form.Control id='attraction_comments' as="textarea" rows={3} onChange={handleAttractionChange} value={attractionForm.comments}></Form.Control>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Country</Form.Label>
-                            <Form.Select onChange={(event) => { onChangeCountryList(event.target.value) }} value={attractionCountryId}>
+                            <Form.Select id='attraction_countryid' onChange={handleAttractionChange} value={attractionForm.country_id}>
                                 <option>Choose country</option>
                                 <option value="176">Peru</option>
                                 <option value="103">India</option>
@@ -113,7 +147,7 @@ function AttractionCreate() {
                         </Form.Group>
                         <Form.Group className='mb-2'>
                             <Form.Label>City</Form.Label>
-                            <Form.Select onChange={(event) => { setAttractionCityId(event.target.value) }} value={attractionCityId}>
+                            <Form.Select id='attraction_cityid' onChange={handleAttractionChange} value={attractionForm.city_id}>
                                 <option>Choose city</option>
                                 {lstCities.map((city, idx) => 
                                     <option key={idx} value={city.id}>{city.name}</option>
