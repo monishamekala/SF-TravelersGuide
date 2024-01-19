9d17197b5327b9c5c6e5e2dd72168a3504580049
monishamekala <monishasmekala@gmail.com>
2024-01-19 01:32:34 -0800

    Added video to the home page #33

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
+import homeVideo from '../../assets/videos/CRUD Video.mp4';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Ratio from 'react-bootstrap/Ratio';
+
 function Home() {
     return (
-        <h1>Welcome, this is Home page</h1>
+        <div style={{ width: "100%", height: 'auto' }}>
+            <Ratio aspectRatio="16x9">
+                <video autoPlay loop muted playsInline>
+                    <source src={homeVideo} type="video/mp4"/>
+                </video>
+            </Ratio>
+            <Container className='my-4'>
+                <Row className="justify-content-md-center">
+                    <Col md="auto"><Button variant="outline-secondary">City</Button></Col>
+                    <Col md="auto"><Button variant="outline-secondary">Country</Button></Col>
+                    <Col md="auto"><Button variant="outline-secondary">Attraction</Button></Col>
+                </Row>
+            </Container>
+        </div>
     );
 }
 
